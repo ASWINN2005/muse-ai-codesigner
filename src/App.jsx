c91c576bcf27c1457a1a9c498c482d87cb6266f0
K Aswin <119060900+ASWINN2005@users.noreply.github.com>
2026-03-05 15:48:40 +0530

    Introduce prompt state for user input handling
    Added prompt state to manage user input for CriticPanel.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-
 import Navbar from "./components/Navbar";
 import PromptInput from "./components/PromptInput";
 import DesignCanvas from "./components/DesignCanvas";
@@ -8,18 +7,40 @@ import CriticPanel from "./components/CriticPanel";
 function App() {
   const [imageUrl, setImageUrl] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
+  // Added this state so CriticPanel can see what you typed
+  const [prompt, setPrompt] = useState(""); 
 
   return (
-    
-<div className="grid grid-cols-12 grid-rows-12 gap-6 p-6 bg-[#1E1F20]">
+    /* The grid layout you built is excellent for a dashboard! */
+    <div className="grid grid-cols-12 grid-rows-12 gap-6 p-6 bg-[#1E1F20] min-h-screen">
 
-    <div className="col-span-12 row-span-1"><Navbar/></div>
-    <div className="col-span-6 row-span-4 col-start-1 row-start-2"><PromptInput setImageUrl={setImageUrl} setIsLoading={setIsLoading}/></div>
-    <div className="col-span-6 row-span-10 col-start-7 row-start-2"><DesignCanvas imageUrl={imageUrl} isLoading={isLoading}/></div>
-    <div className="col-span-6 row-span-5 col-start-1 row-start-6"><CriticPanel imageUrl={imageUrl} setImageUrl={setImageUrl} prompt={prompt}/> </div>
+      <div className="col-span-12 row-span-1">
+        <Navbar />
+      </div>
 
-</div>
-    
+      <div className="col-span-6 row-span-4 col-start-1 row-start-2">
+        {/* Pass setPrompt here so we can store the user's input */}
+        <PromptInput 
+          setImageUrl={setImageUrl} 
+          setIsLoading={setIsLoading} 
+          setPrompt={setPrompt} 
+        />
+      </div>
+
+      <div className="col-span-6 row-span-10 col-start-7 row-start-2">
+        <DesignCanvas imageUrl={imageUrl} isLoading={isLoading} />
+      </div>
+
+      <div className="col-span-6 row-span-5 col-start-1 row-start-6">
+        {/* Now 'prompt' is defined and won't cause an error */}
+        <CriticPanel 
+          imageUrl={imageUrl} 
+          setImageUrl={setImageUrl} 
+          prompt={prompt} 
+        /> 
+      </div>
+
+    </div>
   );
 }
 
